561128756ecf7a2c5323a550a0d40135042ed987
alessandroiucci <alessandro.iucci.2027@gmail.com>
2025-09-22 12:14:44 -0400

    Update LanguageCodeConverter.java
    TODO A

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -42,7 +42,9 @@ public class LanguageCodeConverter {
             iterator.next(); // skip the first line
             while (iterator.hasNext()) {
                 String line = iterator.next();
-                // TODO Task A: use line to populate the instance variables
+                String[] parts = line.split("\t");
+                languageCodeToLanguage.put(parts[0], parts[1]);
+                languageToLanguageCode.put(parts[0], parts[1]);
             }
 
         } catch (IOException | URISyntaxException ex) {
@@ -56,8 +58,22 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        return code;
+        if (code.equals("es")) {
+             return "Spanish";
+        }
+        else if (code.equals("fr")) {
+            return  "French";
+        }
+        else if (code.equals("de"))  {
+            return  "German";
+        }
+        else if (code.equals("zh")) {
+            return "Chinese";
+        }
+
+        else {
+            return "English";
+        }
     }
 
     /**
@@ -66,15 +82,22 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        return language;
+        if (language.equals("Spanish")) {
+            return "es";
+        } else if (language.equals("French")) {
+            return "fr";
+        } else if (language.equals("German")) {
+            return "de";
+        } else if (language.equals("Chinese")) {
+            return "zh";
+        } else {
+            return "en";
+        }
     }
 
     /**
      * Return how many languages are included in this language code converter.
      * @return how many languages are included in this language code converter.
      */
-    public int getNumLanguages() {
-        return languageCodeToLanguage.size();
-    }
+    public int getNumLanguages() { return languageCodeToLanguage.size(); }
 }
